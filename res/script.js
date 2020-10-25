7694c75257fbc3ff67ad5a9bafc843f563f2ee16
telliskivi <kevin.telliskivi@gmail.com>
2020-10-25 16:36:42 +0200

    loadInfo

--- res/script.js
+++ res/script.js
@@ -1,3 +1,16 @@
-function loadPost() {
+function loadInfo() {
+    data = 0;
+    data = $.getJSON('https://private-anon-4d68465a6f-wad20postit.apiary-mock.com/users/1', data)
+    console.log(data)
     return $.get('https://private-anon-4d68465a6f-wad20postit.apiary-mock.com/users/1')
+}
+loadInfo()
+
+
+function loadPost() {
+    return $.get('https://private-anon-4d68465a6f-wad20postit.apiary-mock.com/posts')
+}
+
+function loadProfile() {
+    return $.get('https://private-anon-4d68465a6f-wad20postit.apiary-mock.com/profiles')
 }
