7df1ebb675c9fbaeac707387f7638bc6938d9c02
telliskivi <kevin.telliskivi@gmail.com>
2020-11-22 19:37:52 +0200

    navbar and dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <header>
-
+      <Navbar />
     </header>
   </div>
 </template>
 
 <script>
-
-
+import Navbar from './components/Navbar';
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    
+    Navbar
   }
 }
 </script>
@@ -22,17 +21,13 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-
 }
-
 body {
   font-family: 'montserrat', sans-serif;
 }
-
 header {
   width: 100vw;
   background-color: #222;
   padding: 15px;
 }
 </style>
-
