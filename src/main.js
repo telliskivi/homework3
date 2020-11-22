224bfa6931b0c1f32cb97d759dd97c2325262ebd
telliskivi <kevin.telliskivi@gmail.com>
2020-11-22 16:46:17 +0200

    should work now

--- src/main.js
+++ src/main.js
@@ -1,8 +1,3 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp({}).mount('');
