4c9ce8abdd10ef63f9084878d4b0b070339ca334
telliskivi <kevin.telliskivi@gmail.com>
2020-11-22 03:15:25 +0200

    setup

--- res/posts.js
+++
@@ -1,8 +0,0 @@
-function LoadPostInfo(){
-    return $.get('https://private-anon-fe2f4e1869-wad20postit.apiary-mock.com/posts'), function(response){
-        $("id").html(response);
-        log("success")
-
-
-    }
-}
